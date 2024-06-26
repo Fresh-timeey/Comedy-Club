3869b6db900ba798a93c2aad8686c11321d17493
Puteshestvie <puttikzepp@gmail.com>
2024-06-26 15:05:55 +0300

    Add files via upload

--- mainwindow.h
+++ mainwindow.h
@@ -2,22 +2,30 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTimer>
+#include <QTableWidget>
+#include <QDateTime>
 
-QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
 }
-QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void updateDateTime();
 
 private:
     Ui::MainWindow *ui;
+    QTimer *timer;
+    QTableWidget *tableWidget;
+    QString dateTimeToWords(const QDateTime &dateTime);
 };
+
 #endif // MAINWINDOW_H
